a7614b19436f8dabd270b9abf569b0bf7f3f9d2c
etrittahiri <tahirietrit@gmail.com>
2015-06-27 18:02:04 +0200

    Second submit

--- app/src/main/java/fragments/PlayerFragment.java
+++ app/src/main/java/fragments/PlayerFragment.java
@@ -2,6 +2,7 @@ package fragments;
 
 import android.content.DialogInterface;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -18,8 +19,8 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import com.tahirietrit.spotifystreamerstage2.R;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -61,46 +62,128 @@ public class PlayerFragment extends DialogFragment implements MediaPlayer.OnBuff
     private ArrayList<TopTrack> tracksArrayList = new ArrayList<>();
     private int position;
 
-    private final Handler handler = new Handler();
+    private Handler handler ;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.dialog_player, container, false);
         ButterKnife.inject(this, v);
+
+        handler = new Handler();
+
         position = getArguments().getInt("position");
         tracksArrayList = getArguments().getParcelableArrayList("topTracks");
-        Picasso.with(getActivity()).load(tracksArrayList.get(position).albumThumbnail).into(albumThumbnailPlayer);
         mediaPlayer = new MediaPlayer();
-
         mediaPlayer.setOnBufferingUpdateListener(this);
         mediaPlayer.setOnCompletionListener(this);
 
-        try {
-            mediaPlayer.setDataSource(tracksArrayList.get(position).trackPreview);
-            mediaPlayer.prepare();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        mediaPlayer.start();
-        playTrack.setImageResource(android.R.drawable.ic_media_pause);
-        primarySeekBarProgressUpdater();
-        mediaFileLengthInMilliseconds = mediaPlayer.getDuration();
-
-
+        updatePlayer(position, false);
         trackSeekBar.setOnTouchListener(this);
 
-
-        trackSeekBar.setMax(mediaFileLengthInMilliseconds);
-        String seconds = String.valueOf((mediaFileLengthInMilliseconds % 60000) / 1000);
-
-        String minutes = String.valueOf(mediaFileLengthInMilliseconds / 60000);
-        lengthTextView.setText("" + minutes + ":" + seconds);
-        artistNamePlayer.setText(tracksArrayList.get(position).artistName);
-        trackNamePlayer.setText(tracksArrayList.get(position).trackName);
-        albumNamePlayer.setText(tracksArrayList.get(position).albumName);
-
         return v;
+    }
+
+    public void updatePlayer(final int position, boolean isPlay) {
+
+        if (!isPlay) {
+
+
+            playTrack.setImageResource(android.R.drawable.ic_media_pause);
+
+            artistNamePlayer.setText(tracksArrayList.get(position).artistName);
+            albumNamePlayer.setText(tracksArrayList.get(position).albumName);
+            trackNamePlayer.setText(tracksArrayList.get(position).trackName);
+
+            Picasso.with(getActivity()).load(tracksArrayList.get(position).albumThumbnail).resize(400, 400).centerCrop().into(albumThumbnailPlayer);
+
+
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+
+                    try {
+
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(tracksArrayList.get(position).trackPreview);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                        mediaFileLengthInMilliseconds = mediaPlayer.getDuration();
+                        seekBarProgressUpdater();
+
+                    } catch (Exception ex) {
+
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+
+                    lengthTextView.setText(getStringFormat(mediaFileLengthInMilliseconds));
+                    progresTextView.setText("0:00");
+
+                    super.onPostExecute(aVoid);
+                }
+            }.execute();
+
+
+        } else {
+
+            if (mediaPlayer.isPlaying()) {
+
+                mediaPlayer.pause();
+                playTrack.setImageResource(android.R.drawable.ic_media_play);
+                seekBarProgressUpdater();
+
+            } else {
+
+                mediaPlayer.start();
+                playTrack.setImageResource(android.R.drawable.ic_media_pause);
+                seekBarProgressUpdater();
+            }
+        }
+
+
+    }
+
+    private void seekBarProgressUpdater() {
+
+        if (mediaPlayer != null) {
+            trackSeekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaFileLengthInMilliseconds) * 100));
+
+            if (mediaPlayer.isPlaying()) {
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mediaPlayer != null) {
+                            seekBarProgressUpdater();
+                            progresTextView.setText(getStringFormat(mediaPlayer.getCurrentPosition()));
+                        }
+                    }
+                }, 100);
+            }
+        }
+    }
+
+    @OnClick(R.id.previousTrack)
+    public void setOnClickListenerPrevious() {
+        position--;
+        position = (position < 0) ? tracksArrayList.size() - 1 : position;
+        updatePlayer(position, false);
+    }
+
+    @OnClick(R.id.playTrack)
+    public void setOnClickListenerPlayPause() {
+        updatePlayer(position, true);
+    }
+
+    @OnClick(R.id.nextTrack)
+    public void setOnClickListenerNext() {
+        position++;
+        position = (position == tracksArrayList.size()) ? 0 : position;
+        updatePlayer(position, false);
     }
 
     @Override
@@ -110,7 +193,7 @@ public class PlayerFragment extends DialogFragment implements MediaPlayer.OnBuff
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        playNextTrack();
+        setOnClickListenerNext();
     }
 
     @Override
@@ -123,83 +206,19 @@ public class PlayerFragment extends DialogFragment implements MediaPlayer.OnBuff
         return false;
     }
 
-    private void primarySeekBarProgressUpdater() {
-        trackSeekBar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaFileLengthInMilliseconds) * 100)); // This math construction give a percentage of "was playing"/"song length"
-       if(mediaPlayer != null) {
-           if (mediaPlayer.isPlaying()) {
-               Runnable notification = new Runnable() {
-                   public void run() {
-                       if(mediaPlayer != null) {
-                           primarySeekBarProgressUpdater();
-                           String seconds = String.valueOf((mediaPlayer.getCurrentPosition() % 60000) / 1000);
-                           String minutes = String.valueOf(mediaPlayer.getCurrentPosition() / 60000);
-                           if ((mediaPlayer.getCurrentPosition() % 60000) / 1000 < 10) {
-                               progresTextView.setText("" + minutes + ":0" + seconds);
-                           } else {
-                               progresTextView.setText("" + minutes + ":" + seconds);
-                           }
-                       }
-                   }
-               };
-               handler.postDelayed(notification, 1000);
-           }
-       }
-    }
-
-    @OnClick(R.id.playTrack)
-    public void playPauseTrack() {
-        if (!mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-            playTrack.setImageResource(android.R.drawable.ic_media_pause);
-            primarySeekBarProgressUpdater();
-        } else {
-            mediaPlayer.pause();
-            playTrack.setImageResource(android.R.drawable.ic_media_play);
-            primarySeekBarProgressUpdater();
-        }
-    }
-
-    @OnClick(R.id.nextTrack)
-    public void playNextTrack() {
-        position++;
-        position = (position == tracksArrayList.size()) ? 0 : position;
-        changeTrack(position);
-        primarySeekBarProgressUpdater();
-    }
-
-    @OnClick(R.id.previousTrack)
-    public void playPreviewsTrack() {
-        position--;
-        position = (position < 0) ? tracksArrayList.size() - 1 : position;
-        changeTrack(position);
-        primarySeekBarProgressUpdater();
-
-    }
-
-    public void changeTrack(int position) {
-        try {
-            mediaPlayer.reset();
-            mediaPlayer.setDataSource(tracksArrayList.get(position).trackPreview);
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Picasso.with(getActivity()).load(tracksArrayList.get(position).albumThumbnail).into(albumThumbnailPlayer);
-        artistNamePlayer.setText(tracksArrayList.get(position).artistName);
-        trackNamePlayer.setText(tracksArrayList.get(position).trackName);
-        albumNamePlayer.setText(tracksArrayList.get(position).albumName);
+    private String getStringFormat(int milisecond) {
+        return String.format("%d:%02d", TimeUnit.MILLISECONDS.toMinutes(milisecond) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milisecond)),
+                TimeUnit.MILLISECONDS.toSeconds(milisecond) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milisecond)));
     }
 
     @Override
     public void onDismiss(DialogInterface dialog) {
         super.onDismiss(dialog);
-
-        try{
+        try {
             mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
-        }catch (Exception e){
+        } catch (Exception ex) {
 
         }
     }
